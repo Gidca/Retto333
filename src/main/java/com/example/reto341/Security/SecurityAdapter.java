c71e16d87fdcb29ef084831105e7970b8857da45
Gidca <129134883+Gidca@users.noreply.github.com>
2023-06-10 10:21:38 -0500

    Correcciones Reto

--- src/main/java/com/example/reto341/Security/SecurityAdapter.java
+++ src/main/java/com/example/reto341/Security/SecurityAdapter.java
@@ -2,11 +2,13 @@ package com.example.reto341.Security;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableWebSecurity
 public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
